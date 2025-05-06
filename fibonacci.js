34b6dd195e05ab7546a7700b0a5c36e26b9ead02
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-05-06 13:14:28 -0400

    Complete lesson

--- fibonacci.js
+++ fibonacci.js
@@ -2,19 +2,17 @@
 
 // Iteration
 function fibs(n) {
-
     const sequence = [0, 1];
     if (n <= 2) {
         console.log("Invalid number.");
         return;
     }
-
     for (let i = 2; i < n; i++) {
         sequence[i] = sequence[i - 1] + sequence[i - 2];
     }
-
     return sequence;
 }
+
 console.log(fibs(8));
 
 // Recursion
